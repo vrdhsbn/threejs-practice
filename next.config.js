341803ccff895fcb28dd5572c32a353fcad0c1dc
Naoki Komazawa <komazawa@ekzm.co.jp>
2023-06-30 15:05:50 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
-const prefixPath = !isProd ? '/threejs-practice' : ''
+const prefixPath = isProd ? '/threejs-practice' : ''
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
